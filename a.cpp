939f236c4fae08e9d8d4f9f8b5b837e4032a8352
chaby <chabudaisanta@gmail.com>
2025-08-22 19:35:29 +0900

    Add initial commit

---
+++ a.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include "gwen/types.hpp"
+
+int main() {
+    std::cout << "NekomugiTororo\n";
+    return 0;
+}
---
+++ src/gwen/types.hpp
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <cstdint>
+#include <stdfloat>
+
+namespace gwen {
+
+using i32 = int32_t;
+using i64 = int64_t;
+using i128 = __int128_t;
+using u32 = uint32_t;
+using u64 = uint64_t;
+using u128 = __uint128_t;
+
+
+} // namespace gwen
