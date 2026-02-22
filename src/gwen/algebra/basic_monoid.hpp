8e4cf7cef6fde359dc836f68312ac9fa867e20ca
chaby <chabudaisanta@gmail.com>
2026-02-22 16:08:34 +0900

    no_op_monoid

--- src/gwen/algebra/basic_monoid.hpp
+++ src/gwen/algebra/basic_monoid.hpp
@@ -1,9 +1,18 @@
 #pragma once
 #include <algorithm>
-#include <numeric>
 #include <limits>
+#include <numeric>
 namespace gwen {
 
+template <typename T> struct no_op_monoid {
+    using S = T;
+    static S op(S, S) { return e(); }
+    static S e() { return S(); }
+    using F = T;
+    static S mapping(F, S) { return e(); }
+    static F composition(F, F) { return id(); }
+    static F id() { return F(); }
+};
 template <typename T> struct sum_monoid {
     using S = T;
     static S op(S a, S b) { return a + b; }
@@ -32,29 +41,21 @@ template <typename T> struct minmax_monoid {
     struct S {
         i32 min, max;
     };
-    static S op(S a, S b) {
-        return { std::min(a.min, b.min), std::max(a.max, b.max) };
-    }
-    static S e() {
-        return { std::numeric_limits<T>::min(), std::numeric_limits<T>::max() };
-    }
+    static S op(S a, S b) { return {std::min(a.min, b.min), std::max(a.max, b.max)}; }
+    static S e() { return {std::numeric_limits<T>::min(), std::numeric_limits<T>::max()}; }
 };
 
 template <typename T> struct gcd_monoid {
     using S = T;
-    static S op(S a, S b) {
-        return std::gcd(a, b);
-    }
+    static S op(S a, S b) { return std::gcd(a, b); }
     static S e() { return 0; }
 };
 
-template<typename T> struct affine_monoid {
+template <typename T> struct affine_monoid {
     struct S {
         T a, b;
     };
-    static S op(S f, S g) {
-        return { f.a * g.a, f.a * g.b + f.b };
-    }
+    static S op(S f, S g) { return {f.a * g.a, f.a * g.b + f.b}; }
     static S e() { return {1, 0}; }
 };
 
