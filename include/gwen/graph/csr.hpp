cc303b18ccb571b8723b97cb4071a7dbea7a3703
chaby <chabudaisanta@gmail.com>
2026-06-17 20:55:26 +0900

    refactor(graph): optimize csr graph building and introduce edge_concept

--- include/gwen/graph/csr.hpp
+++ include/gwen/graph/csr.hpp
@@ -1,48 +1,57 @@
 #pragma once
-// 書き直す
+
 #include <span>
 #include <vector>
 
+#include "gwen/graph/edge.hpp"
 #include "gwen/types.hpp"
 
 namespace gwen {
 
-template <typename Edge> struct csr {
+template <edge_concept Edge> struct csr {
     std::vector<i32> start;
     std::vector<Edge> elist;
 
     csr() = default;
     csr(i32 n, const std::vector<Edge>& edges, bool directed = true) : start(n + 1, 0) {
-        std::vector<Edge> tmp;
         if (directed) {
-            tmp = edges;
-        }
-        else {
-            tmp.reserve(edges.size() * 2);
             for (const auto& e : edges) {
-                tmp.push_back(e);
+                start[e.u + 1]++;
+            }
+            for (i32 i = 1; i <= n; ++i) {
+                start[i] += start[i - 1];
+            }
+            elist.resize(edges.size());
+            auto counter = start;
+            for (const auto& e : edges) {
+                elist[counter[e.u]++] = e;
+            }
+        } else {
+            for (const auto& e : edges) {
+                start[e.u + 1]++;
+                start[e.v + 1]++;
+            }
+            for (i32 i = 1; i <= n; ++i) {
+                start[i] += start[i - 1];
+            }
+            elist.resize(edges.size() * 2);
+            auto counter = start;
+            for (const auto& e : edges) {
+                elist[counter[e.u]++] = e;
                 Edge rev = e;
                 rev.u = e.v;
                 rev.v = e.u;
-                tmp.push_back(rev);
+                elist[counter[rev.u]++] = rev;
             }
-        }
-        for (const auto& e : tmp) {
-            start[e.u + 1]++;
-        }
-        for (i32 i = 1; i <= n; ++i) {
-            start[i] += start[i - 1];
-        }
-        elist.resize(tmp.size());
-        auto counter = start;
-        for (const auto& e : tmp) {
-            elist[counter[e.u]++] = e;
         }
     }
 
     i32 size() const { return static_cast<i32>(start.size()) - 1; }
     i32 num_edges() const { return static_cast<i32>(elist.size()); }
 
+    std::span<Edge> operator[](i32 v) {
+        return std::span<Edge>(elist).subspan(start[v], start[v + 1] - start[v]);
+    }
     std::span<const Edge> operator[](i32 v) const {
         return std::span<const Edge>(elist).subspan(start[v], start[v + 1] - start[v]);
     }
