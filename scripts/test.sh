2e592af529632b10d4f5f215edbcc1f66821b122
chaby <chabudaisanta@gmail.com>
2026-07-13 23:18:05 +0900

    chore: test.shがテストログをコピーして保存するように修正

--- scripts/test.sh
+++ scripts/test.sh
@@ -9,4 +9,14 @@ echo "==> Building tests (via scripts/build.sh) <=="
 
 echo "==> Running unit tests <=="
 cd .build
+
+set +e
 ctest --output-on-failure
+EXIT_CODE=$?
+set -e
+
+# CTestが自動生成する詳細ログ（標準出力・標準エラー出力を含む）をコピー
+cp Testing/Temporary/LastTest.log ../test.log
+echo "==> Test log saved to ./test.log <=="
+
+exit $EXIT_CODE
